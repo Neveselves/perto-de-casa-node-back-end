0a7152f707fb853767562bf4857e6e50538b8716
Neveselves <elvesgols033@gmail.com>
2025-01-12 15:03:59 -0300

    atualização correta da implementação mercado pago

--- src/services/mercadoPagoService.js
+++ src/services/mercadoPagoService.js
@@ -65,15 +65,13 @@ class MercadoPagoService {
 
   async handleWebhook(data) {
     if (data.type === "payment") {
-      // const client = new MercadoPagoConfig({
-      //   accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
-      //   options: { timeout: 5000 },
-      // });
+      const client = new MercadoPagoConfig({
+        accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
+        options: { timeout: 5000 },
+      });
 
-      // const preference = new Payment(client);
-      // const payment = await preference.get(data.data.id);
-
-      const payment = await mercadopago.Payment.get(data.data.id);
+      const instancePayment = new Payment(client);
+      const payment = await instancePayment.get({ id: data.data.id });
 
       const order = await Order.findById(payment.body.external_reference);
       if (!order) {
