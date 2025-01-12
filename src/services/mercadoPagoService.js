8a8998cb2fd6f50532b7b87877e9a58930d8d49a
Neveselves <elvesgols033@gmail.com>
2025-01-12 14:44:48 -0300

    arualiza webhook

--- src/services/mercadoPagoService.js
+++ src/services/mercadoPagoService.js
@@ -65,13 +65,15 @@ class MercadoPagoService {
 
   async handleWebhook(data) {
     if (data.type === "payment") {
-      const client = new MercadoPagoConfig({
-        accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
-        options: { timeout: 5000 },
-      });
+      // const client = new MercadoPagoConfig({
+      //   accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN,
+      //   options: { timeout: 5000 },
+      // });
 
-      const preference = new Payment(client);
-      const payment = await preference.get(data.data.id);
+      // const preference = new Payment(client);
+      // const payment = await preference.get(data.data.id);
+
+      const payment = await mercadopago.Payment.findById(data.data.id);
 
       const order = await Order.findById(payment.body.external_reference);
       if (!order) {
